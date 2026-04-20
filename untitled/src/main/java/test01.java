42b7c9b98684cdb5f2a49d3fd9a3a8489b9f7677
Stevexx985 <steveteacher@163.com>
2026-04-20 20:35:50 +0800

    改了什么内容

--- untitled/src/main/java/test01.java
+++ untitled/src/main/java/test01.java
@@ -1,5 +1,6 @@
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("你好 单欣");
+        System.out.println("你好 姜佳林");
+        System.out.println("单欣 你好");
     }
 }
